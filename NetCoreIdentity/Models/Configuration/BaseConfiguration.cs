243fe24af2cb2188eb288e66c14c033e4a2c2ac4
chnsenel@gmail.com <chnsenel@gmail.com>
2024-03-12 23:13:35 +0300

    Add project files.

---
+++ NetCoreIdentity/Models/Configuration/BaseConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NetCoreIdentity.Models.Interfaces;
+
+namespace NetCoreIdentity.Models.Configuration
+{
+    public abstract class BaseConfiguration<T> : IEntityTypeConfiguration<T> where T : class, IEntity
+    {
+        public virtual void Configure(EntityTypeBuilder<T> builder)
+        {
+            builder.Property(x => x.CreatedDate).HasColumnName("YaratilmaTarihi");
+        }
+    }
+}
---
+++ NetCoreIdentity/Models/Entities/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using NetCoreIdentity.Models.Enums;
+using NetCoreIdentity.Models.Interfaces;
+
+namespace NetCoreIdentity.Models.Entities
+{
+    public abstract class BaseEntity : IEntity
+    {
+        public BaseEntity()
+        {
+            CreatedDate = DateTime.Now;
+            Status = DataStatus.Inserted;
+        }
+        public int ID { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+        public DataStatus Status { get; set; }
+    }
+}
