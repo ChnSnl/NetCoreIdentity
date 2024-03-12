c56c90e81166a8c3a134427f8ac41aeaa1f1610a
chnsenel@gmail.com <chnsenel@gmail.com>
2024-03-12 23:37:20 +0300

    MyContext Class'ı tamamlanmıştır.

--- NetCoreIdentity/Program.cs
+++ NetCoreIdentity/Program.cs
@@ -1,9 +1,15 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using NetCoreIdentity.Models.ContextClasses;
+using NetCoreIdentity.Models.Entities;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+builder.Services.AddDbContextPool<MyContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
